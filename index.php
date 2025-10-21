ebc6453e1e27779bdd03ae50ffbea23b141ee887
ovarz <ovarz_03@yahoo.com>
2025-10-21 16:04:29 +0700

    update calendar

--- index.php
+++ index.php
@@ -152,6 +152,7 @@
 	  <div class="calendar-frame img-frame thumb-loading">
 		<iframe title="Calendar" class="lazyload" data-original="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;ctz=Asia%2FJakarta&amp;bgcolor=%237CB342&amp;title=Borussia%20Academy%20Indonesia&amp;src=Mzg4NTlkZTJkODg1MWY3ODI5MThiNmVjNzE0M2Y2NDkzYmZjOTNlZGE4YWJlNjM0Mjg2YzRiMTc3N2ZmYjYwNkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23EF6C00" width="1000" height="600" scrolling="no" frameborder="0" data-gtm-yt-inspected-8="true"></iframe>
 	  </div>
+	  <div class="calendar-note">Typical event calender. if in doubt, please contact Borussia Academy Indonesia for confirmation.</div>
 	  
 	</div>
   </section>
