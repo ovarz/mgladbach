f6faee562cfe2673f7e4fe04ac8ad3841dd11966
ovarz <ovarz_03@yahoo.com>
2025-10-22 12:14:25 +0700

    update

--- index.php
+++ index.php
@@ -144,15 +144,49 @@
   
   
   
-  <section class="section-default section-calendar content-center">
+  <section class="section-default section-herrmann content-center">
+    <div class="bg-blur"></div>
     <div class="site-container">
 	
-	  <div class="section-title">Calendar</div>
-	
-	  <div class="calendar-frame img-frame thumb-loading">
-		<iframe title="Calendar" class="lazyload" data-original="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;ctz=Asia%2FJakarta&amp;bgcolor=%237CB342&amp;title=Borussia%20Academy%20Indonesia&amp;src=Mzg4NTlkZTJkODg1MWY3ODI5MThiNmVjNzE0M2Y2NDkzYmZjOTNlZGE4YWJlNjM0Mjg2YzRiMTc3N2ZmYjYwNkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23EF6C00" width="1000" height="600" scrolling="no" frameborder="0" data-gtm-yt-inspected-8="true"></iframe>
+	  <div class="sph-left">
+	    <h2 class="section-title">Borussia Legend <span style="color:var(--color-4)">Patrick Herrmann</span> Visits Indonesia</h2>
+		<p>In June 2025, Borussia Mönchengladbach legend <b>Patrick <span style="color:var(--color-4)">@flaco7</span> Herrmann</b>  made a special visit to Indonesia as part of the Bundesliga Common Ground project and to spend time at the Borussia M'Gladbach Academy Indonesia.</p>
+		<p>Patrick Herrmann, who played more than a decade in the Bundesliga for Borussia, is loved by fans for his loyalty, pace, and countless decisive goals and assists. His visit brought the spirit of Borussia directly to Jakarta, inspiring young footballers and strengthening the connection between the club and its fans in Southeast Asia.</p>
+		<p>During his stay, Patrick joined community events at the Bundesliga Common Ground in Jakarta, meeting children and local players, signing autographs, and sharing stories from his professional career. He emphasized the importance of teamwork, fair play, and passion—values that Borussia has always stood for.</p>
+		<p>At the <b>Borussia M'Ggladbach Academy Indonesia</b>, Patrick attended training sessions, spoke with coaches, and encouraged our young talents to keep pushing themselves on and off the pitch. He reminded the players that even legends like him once started as kids with big dreams, and that with commitment and hard work, they can also achieve great things.</p>
+		<p>The Academy was honored to welcome such an iconic figure. Patrick Herrmann’s visit marks another milestone in Borussia’s mission to build bridges between Germany and Indonesia through football. His presence will be remembered by everyone who trained, played, and celebrated with him during those unforgettable days in June 2025.</p>
 	  </div>
-	  <div class="calendar-note">Typical event calender. if in doubt, please contact Borussia Academy Indonesia for confirmation.</div>
+	  <div class="sph-right img-frame thumb-loading">
+		<img title="Logo" class="lazyload" data-original="template/img/patrick-herrmann-borussia-mgladbach-academy-indonesia.webp">
+	  </div>
+	  
+	</div>
+  </section>
+  
+  
+  
+  
+  
+  <section class="section-default section-instagram content-center">
+    <div class="site-container">
+	
+	  <?php 
+        $ig_array = array();
+        $ig_array[]=array(
+          'ig_embed'=>'DLo5MDwowTB',
+        );
+        $ig_array[]=array(
+          'ig_embed'=>'DMKA0V-yFJJ',
+        );
+        $ig_array[]=array(
+          'ig_embed'=>'DMPdDahoGDl',
+        );
+        foreach($ig_array as $ig_list){
+	  ?>
+		<div class="ig-box img-frame thumb-loading">
+		  <iframe class="instagram-media instagram-media-rendered lazyload" id="instagram-embed-0" data-original="https://www.instagram.com/p/<?php echo($ig_list['ig_embed'])?>/embed/captioned/" allowtransparency="true" allowfullscreen="true" frameborder="0" height="880" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no"></iframe>
+		</div>
+	  <?php } ?>
 	  
 	</div>
   </section>
@@ -188,6 +222,85 @@
 	  </div>
 	  
 	</div>
+  </section>
+  
+  
+  
+  
+  
+  <section class="section-default section-calendar content-center">
+    <div class="site-container">
+	
+	  <div class="section-title">Calendar</div>
+	
+	  <div class="calendar-frame img-frame thumb-loading">
+		<iframe title="Calendar" class="lazyload" data-original="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;ctz=Asia%2FJakarta&amp;bgcolor=%237CB342&amp;title=Borussia%20Academy%20Indonesia&amp;src=Mzg4NTlkZTJkODg1MWY3ODI5MThiNmVjNzE0M2Y2NDkzYmZjOTNlZGE4YWJlNjM0Mjg2YzRiMTc3N2ZmYjYwNkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23EF6C00" width="1000" height="600" scrolling="no" frameborder="0" data-gtm-yt-inspected-8="true"></iframe>
+	  </div>
+	  <div class="calendar-note">Typical event calender. if in doubt, please contact Borussia Academy Indonesia for confirmation.</div>
+	  
+	</div>
+  </section>
+  
+  
+  
+  
+  
+  <section class="section-default section-methodology content-center">
+    <div class="site-container">
+	
+	  <div class="section-methodology-row section-methodology-right">
+        <div class="section-intro-box">
+          <div class="sib-content">
+		    <h2 class="section-title">Coaching Methodology</h2>
+			<p><b>Borussia M'Gladbach Academy Indonesia has partnered with German Bundesliga club Borussia Mönchengladbach, that is internationally recognized for its outstanding youth development programs.</b></p>
+			<p>So you can be confident that we follow the most modern  training practices. Borussia coaching employs age-specific training programs that focus on skill development, motor coordination and the fundamentals of team play.</p>
+			<p>Borussia players do not stand in a queue to await their turn every few minutes. The small games approach (1v1,2v2,4v4) is particularly crucial and effective for the younger age groups (4 - 9), and ensures maximum engagement and contact with the ball for every player. <b>Borussia training methods</b> encourage players to be creative thinkers and decision makers in a variety of game situations. There are no boring, monotonous, repetitive drills!</p>
+			<p>Once young players are ready, we will provide them with match play experiences without pressure to win.</p>
+			<p>We focus on the players' long term development, not short term results, and our parents are most supportive. We do not want to win at all cost but give our best effort and play as a team.</p>
+          </div>
+          <div class="sib-arrow desktop-only">
+            <?php require ($_SERVER['BMG'].'template/img/icon/gimmick.svg')?>
+          </div>
+        </div>
+	  </div>
+	
+	  <div class="section-methodology-row">
+        <div class="section-intro-box">
+          <div class="sib-content">
+		    <h2 class="section-title">Coaching philosophy</h2>
+			<p><b>At Borussia M'Gladbach Academy Indonesia we believe that we not only train young footballers, but we are also coaching young people in their overall development and growth.</b></p>
+			<p>Enjoying physical activity, meeting challenges and acquiring social skills are all aspects of every day life— important now and in the future !</p>
+			<p>A team sport such a football provides the ideal environment to practise accepting <b>winning & losing</b>, respecting others and maintaining an active, physical lifestyle. These values are now more important than ever.</p>
+			<p>We accept and encourage children of all abilities, and Borussia coaches attend to the different individual needs and talents of every player.</p>
+			<p>It is our priority to guarantee every participant a positive experience in our training sessions and tournaments. <b>Fun, enjoyment & maximum participation</b> are the main ingredients of all Borussia Academy Indonesia sessions.</p>
+          </div>
+          <div class="sib-arrow desktop-only">
+            <?php require ($_SERVER['BMG'].'template/img/icon/gimmick.svg')?>
+          </div>
+        </div>
+	  </div>
+	  
+	</div>
+  </section>
+  
+  
+  
+  
+  
+  <section class="section-default section-training-ground content-center">
+    <div class="stg-info">
+      <h2 class="section-title">TRAINING GROUND</h2>
+	  <p><b>DEUTSCHE SCHULE JAKARTA (DSJ)</b></p>
+	  <p><b>GERMAN SCHOOL JAKARTA</b></p>
+	  <p><b>SEKOLAH JERMAN JAKARTA</b></p>
+	  <p>Jl. Puspa Widya No.8,</p>
+	  <p>BSD City, Kec. Serpong,</p>
+	  <p>Kota Tangerang Selatan, Banten 15322 Indonesia</p>
+    </div>
+
+    <div class="stg-map img-frame thumb-loading">
+      <iframe title="Map" class="lazyload" data-original="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2490.1812252108693!2d106.66715754635472!3d-6.285971546983831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fbc9afc6d467%3A0xf94add4212779b22!2sBorussia%20Academy%20Indonesia!5e0!3m2!1sid!2sid!4v1694231804634!5m2!1sid!2sid" width="1000" height="600" scrolling="no" frameborder="0" data-gtm-yt-inspected-8="true"></iframe>
+    </div>
   </section>
   
   
