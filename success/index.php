e660f76509689dbc9cec0a97c954c539a376792c
ovarz <ovarz_03@yahoo.com>
2025-10-28 14:59:13 +0700

    7

--- success/index.php
+++ success/index.php
@@ -16,11 +16,22 @@
   
   <section class="section-default section-success content-center">
     <style>
-	  .section-success{}
+	  .section-success{background:url(template/img/bg-6.webp) no-repeat fixed center var(--color-3); min-height:calc(100vh - var(--header-height)); min-height:calc(100svh - var(--header-height));}
+	  .success-box{padding:var(--size-5); background-color:var(--bg-2); max-width:610px;}
+	  .success-image{margin-bottom:var(--size-4); margin-top:-175px;}
+	  .success-image-frame{--aspect-ratio:300/270; width:100%; max-width:300px;}
+	  .success-title, .success-desc, .success-button span{text-align:center;}
+	  .success-title{font-weight:bold; text-transform:uppercase; font-size:2rem; color:var(--color-3); margin-bottom:var(--size-1);}
+	  .success-desc{line-height:150%; margin-bottom:var(--size-4);}
 	</style>
     <div class="site-container content-center">
 	
 	  <div class="success-box">
+	    <div class="success-image content-center">
+	      <div class="success-image-frame img-frame thumb-loading">
+            <img title="Success" class="lazyload" data-original="template/img/success.png">
+          </div>
+        </div>
 	    <h1 class="success-title">
           <span class="text-id">Formulir telah berhasil dikirim</span>
           <span class="text-en">Form has been successfully submitted</span>
@@ -31,6 +42,13 @@
           <span class="text-en">Your data has been successfully submitted and is currently being processed. We will contact you shortly to provide further information.</span>
           <span class="text-de">Ihre Daten wurden erfolgreich übermittelt und werden derzeit bearbeitet. Wir werden Sie in Kürze kontaktieren, um weitere Informationen bereitzustellen.</span>
 		</h2>
+		<div class="success-action content-center">
+		  <a title="Back to Home" class="btn success-button" href="">
+            <span class="text-id">Kembali ke Halaman Utama</span>
+            <span class="text-en">Back to Home Page</span>
+            <span class="text-de">Zurück zur Startseite</span>
+		  </a>
+		</div>
 	  </div>
 	  
 	</div>
