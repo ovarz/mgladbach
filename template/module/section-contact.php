cc0c2c53a2094b63afcf7480b498245de6cc6dfc
ovarz <ovarz_03@yahoo.com>
2025-10-31 15:23:21 +0700

    0

--- template/module/section-contact.php
+++ template/module/section-contact.php
@@ -1,70 +1,48 @@
-<section class="section-default section-contact content-center">
+<section class="section-default .section-dark section-contact content-center">
   <div class="site-container">
-    <div class="scu-left">
+    <div class="section-contact-container">
 
-      <div class="scu-box scu-contact-form">
-        <h2 class="section-title">
-          <span class="text-id">Kontak</span>
-          <span class="text-en">Contact</span>
-          <span class="text-de">Contact</span>
-        </h2>
-        <h3 class="section-subtitle">
-          <span class="text-id">Untuk pertanyaan umum saja, bukan untuk pendaftaran.</span>
-          <span class="text-en">For general inquiries only, not intended for registration use</span>
-          <span class="text-de">For general inquiries only, not intended for registration use</span>
-        </h3>
-        <div class="jotform">
-		  <?php $jotform_id='252999107452062'; $jotform_date='1761623287941'; require ($_SERVER['BMG'].'template/module/form-contact.php')?>
-        </div>
-      </div>
-
-    </div>
-    <div class="scu-right">
-
-      <div class="scu-box scu-address">
-        <h2 class="section-title">Borussia Academy Indonesia</h2>
-        <p>PT. German Football Indonesia</p>
-        <p>Roseville Soho Lantai 10</p>
-        <p>BSD City</p>
-        <p>Tangerang Selatan, 15310 Banten</p>
-        <p>Indonesia</p>
-      </div>
-
-      <div class="scu-box scu-address">
+      <div class="scc-box content-center">
         <div class="srs-action">
           <a title="Register" class="btn srs-action-button" href="register/">Register</a>
           <a title="Free Trial" class="btn srs-action-button" href="register/free-trial/">Free Trial</a>
         </div>
       </div>
 
-      <div class="scu-box scu-list">
+      <div class="scc-box scc-address">
+        <h2 class="section-title">Borussia Academy Indonesia</h2>
+        <p>PT. German Football Indonesia</p>
+        <p>Roseville Soho Lantai 10, BSD City</p>
+        <p>Tangerang Selatan, 15310 Banten, Indonesia</p>
+      </div>
+
+      <div class="scc-box scc-list">
         <?php 
           $bai_array = array();
           $bai_array[]=array(
             'bai_title'=>'Email',
             'bai_link'=>'mailto:manager@mgladbachacademy.id',
-            'bai_icon'=>'email',
+            'bai_icon'=>'mail',
             'bai_data'=>'manager@mgladbachacademy.id',
           );
           $bai_array[]=array(
             'bai_title'=>'Whatsapp',
             'bai_link'=>'https://api.whatsapp.com/send/?phone=6281118898205',
-            'bai_icon'=>'whatsapp',
+            'bai_icon'=>'wa',
             'bai_data'=>'+62 8111 8898 205',
           );
           $bai_array[]=array(
             'bai_title'=>'Instagram',
             'bai_link'=>'https://www.instagram.com/mgladbachacademy.id/',
-            'bai_icon'=>'instagram',
+            'bai_icon'=>'ig',
             'bai_data'=>'@mgladbachacademy.id',
           );
           foreach($bai_array as $bai_list){
         ?>
-          <a title="<?php echo($bai_list['bai_title'])?>" class="scu-row" href="<?php echo($bai_list['bai_link'])?>" target="_blank">
-            <div class="scu-icon content-center">
-              <?php require ($_SERVER['BMG'].'template/img/icon/bai-'.$bai_list['bai_icon'].'.svg')?>
+          <a title="<?php echo($bai_list['bai_title'])?>" class="scc-row" href="<?php echo($bai_list['bai_link'])?>" target="_blank">
+            <div class="scc-icon content-center">
+              <?php require ($_SERVER['BMG'].'template/img/icon/footer-outline-'.$bai_list['bai_icon'].'.svg')?>
             </div>
-            <div class="scu-label content-center"><?php echo($bai_list['bai_data'])?></div>
           </a>
         <?php } ?>
       </div>
