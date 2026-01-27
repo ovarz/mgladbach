a01a90f54df7c40a0879facdf50530109639617f
ovarz <ovarz_03@yahoo.com>
2026-01-27 13:11:31 +0700

    0

--- coach/display.php
+++ coach/display.php
@@ -6,6 +6,11 @@
         <h3 class="svr-team"><?php echo $position;?></h3>
       </div>
       <div class="svr-download">
+	    <button title="Download" class="btn svr-download-button open-sticky" aria-popup-button="TrainingPopup">
+          <span class="text-id">Unggah Rencana Latihan</span>
+          <span class="text-en">Upload Training Plan</span>
+          <span class="text-de">Trainingsplan hochladen</span>
+	    </button>
 	    <button title="Download" class="btn svr-download-button open-sticky" aria-popup-button="AbsencePopup">
           <span class="text-id">Absensi</span>
           <span class="text-en">Absence</span>
@@ -28,60 +33,104 @@
 	  </div>
 	  <div class="coach-info">
 	    <div class="coach-row">
-		  <div class="coach-label">Full Name</div>
+		  <div class="coach-label">
+            <span class="text-id">Nama Lengkap</span>
+            <span class="text-en">Full Name</span>
+            <span class="text-de">Vollständiger Name</span>
+		  </div>
 		  <div class="coach-separator">:</div>
 		  <div class="coach-data"><?php echo $name;?></div>
 		</div>
 	    <div class="coach-row">
-		  <div class="coach-label">Birth Date</div>
+		  <div class="coach-label">
+            <span class="text-id">Tanggal Lahir</span>
+            <span class="text-en">Birth Date</span>
+            <span class="text-de">Geburtsdatum</span>
+		  </div>
 		  <div class="coach-separator">:</div>
 		  <div class="coach-data"><?php echo $birtdate;?></div>
 		</div>
 	    <div class="coach-row">
-		  <div class="coach-label">Age</div>
+		  <div class="coach-label">
+            <span class="text-id">Usia</span>
+            <span class="text-en">Age</span>
+            <span class="text-de">Alter</span>
+		  </div>
 		  <div class="coach-separator">:</div>
 		  <div class="coach-data"><?php echo $umur->y;?> years old</div>
 		</div>
 	    <div class="coach-row">
-		  <div class="coach-label">Email</div>
+		  <div class="coach-label">
+            <span class="text-id">Email</span>
+            <span class="text-en">Email</span>
+            <span class="text-de">Email</span>
+		  </div>
 		  <div class="coach-separator">:</div>
 		  <div class="coach-data"><a href="mailto:<?php echo $email;?>"><?php echo $email;?></a></div>
 		</div>
 	    <div class="coach-row">
-		  <div class="coach-label">Phone</div>
+		  <div class="coach-label">
+            <span class="text-id">Telepon</span>
+            <span class="text-en">Phone</span>
+            <span class="text-de">Telefon</span>
+		  </div>
 		  <div class="coach-separator">:</div>
 		  <div class="coach-data"><a href="tel:<?php echo $phone;?>"><?php echo $phone;?></a></div>
 		</div>
 	    <div class="coach-row">
-		  <div class="coach-label">Address</div>
+		  <div class="coach-label">
+            <span class="text-id">Alamat</span>
+            <span class="text-en">Address</span>
+            <span class="text-de">Adresse</span>
+		  </div>
 		  <div class="coach-separator">:</div>
 		  <div class="coach-data"><?php echo $address;?></div>
 		</div>
 	    <div class="coach-row">
-		  <div class="coach-label">Join Date</div>
+		  <div class="coach-label">
+            <span class="text-id">Tanggal Bergabung</span>
+            <span class="text-en">Join Date</span>
+            <span class="text-de">Eintrittsdatum</span>
+		  </div>
 		  <div class="coach-separator">:</div>
 		  <div class="coach-data"><?php echo $joindate;?></div>
 		</div>
 	    <div class="coach-row">
-		  <div class="coach-label">Position</div>
+		  <div class="coach-label">
+            <span class="text-id">Posisi</span>
+            <span class="text-en">Position</span>
+            <span class="text-de">Position</span>
+		  </div>
 		  <div class="coach-separator">:</div>
 		  <div class="coach-data"><?php echo $position;?></div>
 		</div>
 	    <div class="coach-row">
-		  <div class="coach-label">Schedule</div>
+		  <div class="coach-label">
+            <span class="text-id">Jadwal</span>
+            <span class="text-en">Schedule</span>
+            <span class="text-de">Zeitplan</span>
+		  </div>
 		  <div class="coach-separator">:</div>
 		  <div class="coach-data"><?php echo $schedule;?></div>
 		</div>
 	  </div>
 	  
       <div class="training-plan">
-        <div class="section-title">Training Plan</div>
+        <div class="section-title">
+          <span class="text-id">Rencana Latihan</span>
+          <span class="text-en">Training Plan</span>
+          <span class="text-de">Trainingsplan</span>
+		</div>
         <ul class="training-plan-list">
 		  <?php for ($i=1; $i<=4; $i++){ ?>
             <li class="tpl-box">
               <div class="tpl-info">
                 <div class="tpl-title">Training Plan U10 Borussia - January 2026</div>
-                <div class="tpl-desc">Last update : 00 September 2025</div>
+                <div class="tpl-desc">
+                  <span class="text-id">Pembaruan Terakhir : 00 September 2025</span>
+                  <span class="text-en">Last update : 00 September 2025</span>
+                  <span class="text-de">Zuletzt Aktualisiert : 00 September 2025</span>
+				</div>
               </div>
               <div class="tpl-download">
                 <a class="btn tpl-download-button" href="" target="_blank">
