12c793cc1a87c2d8e07ca314330a8cafc5a36019
ovarz <ovarz_03@yahoo.com>
2026-01-30 16:37:02 +0700

    shortcut

---
+++ coach/absence/index.php
@@ -0,0 +1,43 @@
+<?php 
+  $lang='en';
+  $menu='Coach';
+  $site_title='default';
+  require ('../../template/inc/base.php');
+?>
+<?php require ($_SERVER['BMG'].'template/inc/meta.php')?>
+<?php require ($_SERVER['BMG'].'template/inc/header.php')?>
+<div class="rancak-foundation">
+  
+  
+  
+  
+  <section class="section-default section-login content-center">
+    <div class="site-container shortcut-container content-center">
+      <div class="shortcut-box">
+		  <div class="jotform">
+		    <style>
+			  body::-webkit-scrollbar{width:1px;}
+			</style>
+			<iframe
+			  id="JotFormIFrame-260290932274053"
+			  title="Form"
+			  onload="window.parent.scrollTo(0,0)"
+			  allowtransparency="true"
+			  allow="geolocation; microphone; camera; fullscreen; payment"
+			  src="https://form.jotform.com/260290932274053"
+			  frameborder="0"
+			  style="min-width:100%;max-width:100%;height:539px;border:none;"
+			>
+			</iframe>
+			<script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-260290932274053']", "https://form.jotform.com/")</script>
+		  </div>
+      </div>
+    </div>
+  </section>
+  
+  
+  
+  
+  
+</div>
+<?php require ($_SERVER['BMG'].'template/inc/base-bottom.php')?>
