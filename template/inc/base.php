6366399ae3cf80b2d3b9eb26ac0d804bc82f38b7
ovarz <ovarz_03@yahoo.com>
2026-02-26 11:49:57 +0700

    Update base.php

--- template/inc/base.php
+++ template/inc/base.php
@@ -4,5 +4,6 @@
 <?php 
   $_SERVER['BMG'] = $_SERVER['DOCUMENT_ROOT'] . '/';  
   $sitename = 'Borussia Mönchengladbach Academy Indonesia';
+  $sitedesc = 'Unlock your potential at Borussia Academy Indonesia. Join our premier football academy in Jakarta. Enhance skills, tactical awareness, and physical conditioning.';
   $anticache = date ('s'.'i'.'H'.'d'.'m'.'Y');
 ?>
